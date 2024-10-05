ecd001a290566c0344751511b1ecfe3c4a64a556
Suraj Pandey <surajpandey7493@gmail.com>
2024-10-05 20:17:46 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './Calculator/App';
 
 test('renders learn react link', () => {
   render(<App />);
